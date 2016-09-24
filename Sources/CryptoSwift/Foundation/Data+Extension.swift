629ceffc9a24c1205772e77547003a773131acfb
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-09-24 14:17:27 -0500

    Revert "Removed conflicting Data.bytes extension"
    This reverts commit 2e233c3ff97955c5d3e9e72f0f50153b7aa23e96.

--- Sources/CryptoSwift/Foundation/Data+Extension.swift
+++ Sources/CryptoSwift/Foundation/Data+Extension.swift
@@ -78,6 +78,10 @@ extension Data {
 }
 
 extension Data {
+
+    public var bytes: Array<UInt8> {
+        return Array(self)
+    }
     
     public func toHexString() -> String {
         return self.bytes.toHexString()
